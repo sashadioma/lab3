dcc9cebff9185f8f312a511bd9b69c8516f90bc3
sashadioma <ivolkovitskiy215@gmail.com>
2022-03-24 21:35:32 +0200

    hello

---
+++ 5.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h> 
+#include <conio.h>
+using namespace std;
+int main() 
+{
+	float x;
+	cout <<"Vvedit x:";
+	cin >>x;
+	x=sin(x)*pow(cos(x),2)*sin(x+1.4)/0.85+7.14;
+	cout <<"x="<<x;
+	return 0;
+}
